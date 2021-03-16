e2aa097c74c3afc6f688a09f17a21fc1f8a196df
Tom Zhang <treez.zhang@gmail.com>
2021-03-15 17:09:21 -0700

    Move to JDK 8

---
+++ src/test/java/org/audiorec/RecorderTest.java
@@ -0,0 +1,38 @@
+package org.audiorec;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecorderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAudioFormat() {
+    }
+
+    @Test
+    void captureAudio() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void save() {
+    }
+}
